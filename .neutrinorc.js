7715c58cb21118919b7d5e06e73c1c955e662631
David Evans <davidje13@users.noreply.github.com>
2020-08-29 23:11:14 +0100

    Use NPM packages

--- .neutrinorc.js
+++ .neutrinorc.js
@@ -1,5 +1,5 @@
-const typescript = require('neutrino-typescript');
-const typescriptLint = require('neutrino-typescript-eslint');
+const typescript = require('neutrinojs-typescript');
+const typescriptLint = require('neutrinojs-typescript-eslint');
 const node = require('@neutrinojs/node');
 const jest = require('@neutrinojs/jest');
 const eslint = require('@neutrinojs/eslint');
